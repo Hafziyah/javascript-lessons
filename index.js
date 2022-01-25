e49289a53ea4d49d646f169c3fced4711836b501
Hafsat Ajia-Egbeyemi <hafsatajia@gmail.com>
2022-01-25 02:16:55 +0100

    Created the save function

--- index.js
+++ index.js
@@ -4,12 +4,19 @@ let count = 0
 let countEl = document.getElementById("count-el")
 console.log(countEl)
 
+
+ let saveEl = document.getElementById("save-el")
+ console.log (saveEl)
+
+ 
 function increment() {
-    count = count + 1
+    count = count + 1 //same as count += 1
     console.log(count)
     countEl.innerText=count
 }
 
 function save() {
-    console.log(count)
+   let savedN = count + " -"
+   saveEl.innerText += savedN
+   console.log(count)
 }
